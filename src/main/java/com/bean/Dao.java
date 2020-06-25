450bca5971388918eb9905d785be3da4a3cb575a
mywgl <2442997001@qq.com>
2020-06-25 21:58:21 +0800

    添加一个文件

---
+++ src/main/java/com/bean/Dao.java
@@ -0,0 +1,14 @@
+package com.bean;
+
+/**
+ * @Description
+ * @Author WGL
+ * @Date 2020/6/25 21:56
+ * @Version 1.0
+ */
+
+public class Dao {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
